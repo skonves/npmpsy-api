c926159131d4b443986475e5637f1dd16ffeabc8
Steve Konves <stephen.konves@gmail.com>
2017-04-29 12:29:51 -0700

    refactor: abstract neo4j logic

---
+++ src/neo4j-client.js
@@ -0,0 +1,32 @@
+import request from 'superagent';
+
+export default class Neo4jClient {
+	constructor(endpoint, authorization) {
+		this.endpoint = endpoint; // `http://${endpoint}:7474/db/data/transaction/commit`;
+		this.authorization = authorization; //`Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
+	}
+
+	async query(statement, parameters) {
+		const body = {
+			statements: [{
+				statement,
+				parameters,
+				resultDataContents: ['row'],
+				includeStats: false
+			}]
+		};
+
+		// TODO: add circuit breaker
+		const result = await request
+			.post(this.endpoint)
+			.send(body)
+			.set('Authorization', this.authorization)
+			.set('Content-Type', 'application/json');
+
+		if (result.body.errors && result.body.errors.length) {
+			throw result.body.errors;
+		}
+
+		return result.body.results[0];
+	}
+}
--- src/services/search-service.js
+++ src/services/search-service.js
@@ -1,38 +1,23 @@
 import request from 'superagent';
+import Neo4jClient from '../neo4j-client';
 
 const endpoint = `http://${process.env.NEO4J_HOST}:7474/db/data/transaction/commit`;
 const authorization = `Basic ${process.env.NEO4J_AUTH}`;
 
+const client = new Neo4jClient(endpoint, authorization);
+
 export function findPackages(q, offset, limit, callback) {
-	// q = q || '';
-	// offset = offset || 0;
-	// limit = limit || 25;
 
-	const body = {
-		statements: [{
-			statement: `
-			MATCH (p:Package)-[:RELEASED_AS]->(v:Version) WHERE p.id STARTS WITH {q}
-			RETURN DISTINCT p.id AS packageId, collect({name: v.name, ts: v.ts}) AS versions
-			ORDER BY packageId SKIP {offset} LIMIT {limit}`,
-			parameters: { q, offset, limit },
-			'resultDataContents': [
-				'row'
-			],
-			'includeStats': false
-		}]
-	};
+	const statement = `
+		MATCH (p:Package)-[:RELEASED_AS]->(v:Version) WHERE p.id STARTS WITH {q}
+		RETURN DISTINCT p.id AS packageId, collect({name: v.name, ts: v.ts}) AS versions
+		ORDER BY packageId SKIP {offset} LIMIT {limit}`;
 
-	request
-		.post(endpoint)
-		.send(body)
-		.set('Authorization', authorization)
-		.set('Content-Type', 'application/json')
-		.end((err, result) => {
-			if (err) {
-				callback(err);
-			} else if (result.body.results[0] && result.body.results[0].data) {
+	client.query(statement, { q, offset, limit })
+		.then(result => {
+			if (result && result.data) {
 				callback(null, {
-					packages: result.body.results[0].data.map(d => {
+					packages: result.data.map(d => {
 						return {
 							id: d.row[0],
 							versions: d.row[1] ? d.row[1].sort((a, b) => b.ts - a.ts) : []
@@ -42,7 +27,44 @@ export function findPackages(q, offset, limit, callback) {
 			} else {
 				callback();
 			}
-		});
+		})
+		.catch(ex => callback(ex));
+
+	// const body = {
+	// 	statements: [{
+	// 		statement: `
+	// 		MATCH (p:Package)-[:RELEASED_AS]->(v:Version) WHERE p.id STARTS WITH {q}
+	// 		RETURN DISTINCT p.id AS packageId, collect({name: v.name, ts: v.ts}) AS versions
+	// 		ORDER BY packageId SKIP {offset} LIMIT {limit}`,
+	// 		parameters: { q, offset, limit },
+	// 		'resultDataContents': [
+	// 			'row'
+	// 		],
+	// 		'includeStats': false
+	// 	}]
+	// };
+
+	// request
+	// 	.post(endpoint)
+	// 	.send(body)
+	// 	.set('Authorization', authorization)
+	// 	.set('Content-Type', 'application/json')
+	// 	.end((err, result) => {
+	// 		if (err) {
+	// 			callback(err);
+	// 		} else if (result.body.results[0] && result.body.results[0].data) {
+	// 			callback(null, {
+	// 				packages: result.body.results[0].data.map(d => {
+	// 					return {
+	// 						id: d.row[0],
+	// 						versions: d.row[1] ? d.row[1].sort((a, b) => b.ts - a.ts) : []
+	// 					};
+	// 				})
+	// 			});
+	// 		} else {
+	// 			callback();
+	// 		}
+	// 	});
 }
 
 export function getVersions(packageId, offset, limit, callback) {
